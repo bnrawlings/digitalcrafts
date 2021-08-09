e03958efc2b832ab85d7aeab38841a4601cbd2cd
brittney rawlings <bn.rawlings@gmail.com>
2021-08-09 11:52:09 -0500

    Create dictionarypractice.py

---
+++ week2/day1/dictionarypractice.py
@@ -0,0 +1,24 @@
+ramit = {
+  'name': 'Ramit',
+  'email': 'ramit@gmail.com',
+  'interests': ['movies', 'tennis'],
+  'friends': [
+    {
+      'name': 'Jasmine',
+      'email': 'jasmine@yahoo.com',
+      'interests': ['photography', 'tennis']
+    },
+    {
+      'name': 'Jan',
+      'email': 'jan@hotmail.com',
+      'interests': ['movies', 'tv']
+    }
+  ]
+}
+def countFriends():
+    friends_count = str(ramit, ["friends"])
+    for friends_count in ramit:
+        return len["friends"]
+
+print(countFriends)        
+
