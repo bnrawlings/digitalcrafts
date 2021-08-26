64d676fa95c1706b853a286f039ac3495e5d3949
brittney rawlings <bn.rawlings@gmail.com>
2021-08-26 17:20:16 -0500

    update to omdb.js

--- week4/day3/omdb.js
+++ week4/day3/omdb.js
@@ -1,20 +1,32 @@
 // console.log ("I'm connected")
 
+const apiKey = "a8956cc"
+const omdbUrl = `http://www.omdbapi.com/?apikey=${apiKey}`
 
-const omdbUrl = "http://www.omdbapi.com/?apikey=a8956cc"
 
 const movieDeckcon = document.querySelector(".movieDeck")
 const searchButton = document.querySelector(".searchButton")
 
 async function getMovies(){
+    movieDeckcon.innerHTML = "";
+    
+    
     const input = document.querySelector(".inputField").value;
-    const movieData = await fetch(omdbUrl);
+    
+    const movieData = await fetch(omdbUrl + `${input}`);
     const jsonMovies = await movieData.json();
-    console.table(jsonMovies)
-    // const movieImg = document.createElement("img");
-    // movieDeckcon.innerHTML = "";
+    console.log(jsonMovies)
+    
+    
 
-//   for (const movie in jsonMovies.Search)
+  for (const movie in jsonMovies.Search){
+      const movieImg = document.createElement("img");
+      console.log(movie)
+      const movieTitle = document.createElement("h3");
+      movieImg.src = movie.Poster;
+      movieTitle.innerHTML = movie.Title
+      movieDeckcon.append(movieTitle, movieImg)
+  }
 }
 
 getMovies();
