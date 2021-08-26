2e4471a03936586f4afc1d0d720c4cb4c54a48ea
brittney rawlings <bn.rawlings@gmail.com>
2021-08-26 14:37:32 -0500

    Update omdb.js

--- week4/day3/omdb.js
+++ week4/day3/omdb.js
@@ -1,3 +1,20 @@
 // console.log ("I'm connected")
 
 
+const omdbUrl = "http://www.omdbapi.com/?apikey=a8956cc"
+
+const movieDeckcon = document.querySelector(".movieDeck")
+const searchButton = document.querySelector(".searchButton")
+
+async function getMovies(){
+    const input = document.querySelector(".inputField").value;
+    const movieData = await fetch(omdbUrl);
+    const jsonMovies = await movieData.json();
+    console.table(jsonMovies)
+    // const movieImg = document.createElement("img");
+    // movieDeckcon.innerHTML = "";
+
+//   for (const movie in jsonMovies.Search)
+}
+
+getMovies();
