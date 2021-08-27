5c4fb316ebf6f7ac7330d68c5748cbec5118808b
brittney rawlings <bn.rawlings@gmail.com>
2021-08-27 13:51:23 -0500

    updated weather

--- week4/day5/weather.js
+++ week4/day5/weather.js
@@ -6,11 +6,18 @@ const searButton = document.querySelector(".searchButton")
 async function getWeather(){
     const zipCode = document.querySelector(".input").value;
     const apiKey = "appid=0cc6786191441152c418fb0c525bc2f7";
-    const weatherURL = `api.openweathermap.org/data/2.5/weather?zip=${zipCode}&${apiKey}&units=imperial`;
+    const weatherURL = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&${apiKey}&units=imperial`;
     const weatherData = await fetch (weatherURL);
     const jsonWeather = await weatherData.json();
-    console.log(jsonWeather)
-}
+    console.table(jsonWeather.main)
+    const weatherwid = document.createElement("div");
+    bigContain.append(weatherwid)
+    weatherwid.className = "newWidget";
+    const tempature = document.createElement("h4");
+    tempature.innerHTML = jsonWeather.main.temp;
+    newWidget.append(tempature)
+    }
+
 
 searButton.addEventListener("click", ()=>{getWeather();
 })            
