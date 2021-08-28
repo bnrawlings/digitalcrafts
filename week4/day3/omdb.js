2adf3ee16217ccc86f048a17fb552f18b661f91f
brittney rawlings <bn.rawlings@gmail.com>
2021-08-28 16:25:48 -0500

    omdb update

--- week4/day3/omdb.js
+++ week4/day3/omdb.js
@@ -13,20 +13,22 @@ async function getMovies(){
     
     const input = document.querySelector(".inputField").value;
     
-    const movieData = await fetch(omdbUrl + `${input}`);
+    const movieData = await fetch(omdbUrl + `&s=${input}`);
     const jsonMovies = await movieData.json();
     console.log(jsonMovies)
     
     
 
-  // for (const movie in jsonMovies){
-  //     const movieImg = document.createElement("img");
-  //     console.log(movie)
-  //     const movieTitle = document.createElement("h3");
-  //     movieImg.src = movie.Poster;
-  //     movieTitle.innerHTML = movie.Title
-  //     movieDeckcon.append(movieTitle, movieImg)
+  for (const movie in jsonMovies){
+      const movieImg = document.createElement("img");
+      console.log(movie)
+      const movieTitle = document.createElement("h3");
+      movieImg.src = movie.Poster;
+      movieTitle.innerHTML = movie.Title;
+      movieDeckcon.append(movieTitle, movieImg);
+      
   }
-}
+  }
 
-getMovies();
+
+searchButton.addEventListener("click", () => getMovies());
