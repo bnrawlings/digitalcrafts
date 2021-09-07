54379ad8d1214a5242ae0c4746e0187a3be0c01c
brittney rawlings <bn.rawlings@gmail.com>
2021-09-07 13:58:36 -0500

    updated with training exercises complete

--- week6/day1/index2.js
+++ week6/day1/index2.js
@@ -63,10 +63,79 @@
 //   app.listen(3000)
 
 //  problem #5
-const cowsay = require("cowsay");
-const express = require("express");
-const app = express();
+// const cowsay = require("cowsay");
+// const express = require("express");
+// const app = express();
 
-app.delete("/deleteCowUser", (req, res) => {
-    res.send(console.log("cowUser deleted"))});
-  app.listen(3000)
+// app.delete("/deleteCowUser", (req, res) => {
+//     res.send(console.log("cowUser deleted"))});
+//   app.listen(3000)
+
+// Training Exercises (small)
+
+// problem #1
+// const express = require("express");
+// const app = express();
+
+// app.post("/hello", (req,res)=> {
+//     res.send("Hello");
+// })
+// app.listen(3000);
+
+// const express = require ("express");
+// const app = express();
+
+// app.get("/cats", (req,res)=>{
+//     res.send("Meow")
+// })
+// app.listen(3000)
+
+// const express = require("express");
+// const app = express();
+
+// app.put("/dogs", (req,res)=>{
+//     res.send("Woof")
+// })
+// app.listen(3000)
+
+// const express = require("express");
+// const app = express();
+
+// app.delete("/cats_and_dogs", (req,res)=>{
+//     res.send("Dogs and Cats living togther....Mass hysteria!!")
+// })
+// app.listen(3000)
+
+    // problem #2
+    // const express = require("express");
+    // const app = express()
+
+    // app.get("/greet/Luke", (req,res)=>{
+    //     res.send("Hello, Luke!")
+    // })
+    // app.listen(3000)
+
+    // const express = require("express");
+    // const app = express()
+
+    // app.get("/greet/Ahsoka", (req,res)=>{
+    //     res.send("Hello, Ahsoka!")
+    // })
+    // app.listen(3000)
+
+    // const express = require("express");
+    // const app = express()
+
+    // app.get("/greet/Han", (req,res)=>{
+    //     res.send("Hello, Han!")
+    // })
+    // app.listen(3000)
+
+        // problem #3
+        const express = require("express");
+        const app = express();
+
+        app.get("/greet/Tiana", (req,res)=>{
+            res.send(`<h1>Hello,Tiana!<h1>`)
+        })
+        app.listen(3000)
