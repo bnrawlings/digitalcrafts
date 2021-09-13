09b5d99c1a2b87b773b9e3879660162662163c3a
brittney rawlings <bn.rawlings@gmail.com>
2021-09-13 16:58:48 -0500

    HW complete

--- week7/day1/databaseHW/server/index.js
+++ week7/day1/databaseHW/server/index.js
@@ -35,4 +35,34 @@ app.get("/getRestaurant", (req, res) => {
         });
     });
     });
+
+   //CREATE
+   app.post("/addMovie", (req, res) => {
+    // res.send("/addRestaurant");
+    creds.connect((err,client,release) =>{
+        client.query(`INSERT INTO movies (title) VALUES ('${req.body.title}');`, (err, results) => {
+            console.log(err);
+            console.log(results);
+            if (err){
+                res.status(400).send(err.stack);
+            }
+            res.status(200).send(results);
+        });
+      
+    });
+})
+    //READ
+app.get("/getMovies", (req, res) => {
+    creds.connect((err, client,release) => {
+        client.query("SELECT * FROM movies", (err, result) => {
+            console.log(err);
+            console.log(result);
+            if (err){
+                res.status(400).send(err.stack);
+            }
+            res.status(200).send(result);
+        });
+    });
+    });
+
 app.listen(PORT, console.log(`Listening on port ${PORT}`));
