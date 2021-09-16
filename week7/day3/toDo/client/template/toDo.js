d9531702af747e1d40aeb6cd872d13ecdf4979a0
brittney rawlings <bn.rawlings@gmail.com>
2021-09-16 10:59:46 -0500

    todo fullstack complete

--- week7/day3/toDo/client/template/toDo.js
+++ week7/day3/toDo/client/template/toDo.js
@@ -1,18 +1,13 @@
 
 //TO CREATE NEW TASK IN HTML
 const createBtn = document.querySelector(".create_btn")
-
 const addTask = async () => {
     const url = "http://localhost:3006/create_task";
-    const task = document.querySelector(".new_task").value;
-    const prio = document.querySelector(".new_prio").value;
+    const taskTodo = document.querySelector(".new_tasks").value;
+    const priority = document.querySelector(".new_prio").value;
     const newTask = {
-        task,
-        prio,
-    };
-    
-    
-    
+        taskTodo,
+        priority, }; 
     const createTask = await fetch(url, {
         method: "POST",
         mode: "cors",
@@ -23,34 +18,51 @@ const addTask = async () => {
     });
     console.log(createTask)
 };
-
 createBtn.addEventListener("click", () => addTask()) 
 
+//TO REMOVE TASK FROM DATABASE
+const deleteBtn = document.querySelector(".delete_btn")
+const destroyTask = async () => {
+    const taskTodelete = document.querySelector(".delete_id").value;
+    const url = `http://localhost:3006/delete_task/${taskTodelete}`;
+    const deleteTask = await fetch(url, {
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+        },
+        // body: JSON.stringify(deleteTask),
+    })
+    console.log("Your task has been deleted")
+};
+deleteBtn.addEventListener("click", () => destroyTask()) 
 
+//TO UPDATE TASK
+const updateBtn = document.querySelector(".update_btn");
+const updateTask = async () => {
+    const taskUpdateId = document.querySelector(".update_id").value;
+    const url = `http://localhost:3006/update_task/${taskUpdateId}`;
+    const taskUpdate = document.querySelector(".update_tasks").value;
+    const taskPrio = document.querySelector(".update_prio").value;
+    const updatedTask = {
+        taskTodo : taskUpdate,
+        priority : taskPrio,
+    };
+        console.log(updatedTask)
+    const updateList = await fetch(url, {
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+        },
+        body: JSON.stringify(updatedTask),
+    });
+    console.log(updateList)
+};
+updateBtn.addEventListener("click", () => updateTask())    
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//TO RENDER TASK LIST ON HTML
+//TO VIEW TASK LIST ON HTML
 const readBtn = document.querySelector(".read_subBtn")
-
 const getTaskList = async () => {
     const url = "http://localhost:3006/get_task";
     const taskList = await fetch(url, {
@@ -59,7 +71,7 @@ const getTaskList = async () => {
         headers: {
             "Content-type": "application/json; charset=UTF-8",
         }
-    })
+    });
     const json = await taskList.json()
         for (task of json){
             const taskTile = task.taskTodo;
@@ -76,5 +88,4 @@ const getTaskList = async () => {
 };
     console.log(json)
 };
-
 readBtn.addEventListener("click", () => getTaskList())    
