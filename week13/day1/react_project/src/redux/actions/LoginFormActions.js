99a309218690ca9d5e3629801759e6bbdfd2db2f
brittney rawlings <bn.rawlings@gmail.com>
2021-10-21 20:41:40 -0500

    Updated login form actions

--- week13/day1/react_project/src/redux/actions/LoginFormActions.js
+++ week13/day1/react_project/src/redux/actions/LoginFormActions.js
@@ -1,5 +1,9 @@
-import { SET_USER } from "../action-types/loginFormaction-types";
+import { SET_USER, GET_USER } from "../action-types/loginFormaction-types";
 
-export const LoginUser = (dispatch) => {
+
+export const RegisterUser = (dispatch) => {
     dispatch ({type: SET_USER})
+}
+export const LoginUser = (dispatch) => {
+    dispatch ({type: GET_USER})
 }
