04c8fd0935edc09acab99e72b612323b24099442
Brittney Rawlings <bn.rawlings@gmail.com>
2021-10-29 11:13:57 -0400

    contact form added

--- week13/day1/react_project/src/components/Contact.js
+++ week13/day1/react_project/src/components/Contact.js
@@ -5,6 +5,13 @@ export default function Contact() {
     return (
         <ContactDiv className="contact">
             <h1>CONTACT SECTION</h1>
+            <div>
+            <form>
+                <input type="text"/>
+                <input type="text"/>
+                <button >Submit</button>
+            </form>
+            </div>
         </ContactDiv>
     )
 }
--- week13/day1/react_project/src/styled-components/ContactStyled.js
+++ week13/day1/react_project/src/styled-components/ContactStyled.js
@@ -3,3 +3,9 @@ import styled from "styled-components";
 export const ContactDiv = styled.div`
 background-color: #CCB28F;
 `
+export const Form = styled.form`
+display: flex;
+flex-direction: column;
+
+
+`
